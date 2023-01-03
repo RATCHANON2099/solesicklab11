a6ff678b17e9a9a958cd802b774b2b2d88334233
solesicksohigh <solesick@cmu.ac.th>
2023-01-03 17:10:22 +0700

    solesicklab11_3_completed

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,32 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    cout << setprecision(3);
+    ifstream source;
+	ofstream dest;
+	source.open("score.txt");
+	dest.open("completed.txt");
+    string textline;
+    double num=0, avr=0, dvt=0, sum=0, sm=0, cr=0;
+    while(getline(source, textline)){
+        cr=stod(textline);
+        sum=sum+cr;
+        sm+=pow(cr,2);
+        num++;
+    }
+    avr=sum/num;
+    dvt=sqrt(sm/num-pow(avr,2));
+    cout << "Number of data = " << num << endl;
+    cout << "Mean = " << avr << endl;
+    cout << "Standard deviation = " << dvt << endl;
+
+    source.close();
+    dest.close();
+	
+}
